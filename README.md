3129e1866db8f99f9a295f79fb24118e364c8744
haard7 <haardpatel07@gmail.com>
2024-10-02 07:46:30 -0500

    modified clothshop.sql (added image url and productsize)

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-## Branch: `postgres_da_multi_agent`
+## Branch: `feature/1-recommend-purchase-product`
 
 ### Description
 
-- This branch contains the example of multi agent framework using autogen and openai api
-- It contains three agents Data Engineer, Sr. Data Analyst and Product manager. above these three we have user-proxy agent which get the user input and send the request to these agents in sequence, for writting sql query send to data engineer agent, for reviewing the query send to sr. data analyst agent and finally for validation send to product manager agent
-- Note: This is just for example and learning that how any agent can interact with the database but actually we will have different architecture and agents for our use case
-- Here we are using the postgresql database. For ease I have used the clothShop database which is more aligning with our usecase.
-  - Basically this will get the input from user in simple language like "get all the products for gender male and color black" and respond back the json with required output.
+- In this branch currently the development of product recommendation agent is going
+- it should be able to write the data into the database if user wants to purchase the recommended product
 
 ## How to Run
 
@@ -26,11 +23,9 @@
 6. run using below command
    `python -m postgres_da_ai_agent.main --prompt "get all the products for gender Men"`
 
-### Note ⚠️ (Important!):Here this project is using GPT-4 model which is very expensive.
-
 ## Next steps
 
-1. make the code works for `gpt-4o-mini` or `gpt-3.5-turbo`
-2. change the agents to the actual agents like product recommendation, order status etc..
-3. modify the agentic workflow to enable user-proxy agent to decide which agent to give the task for output
+1. should be able to get the context from previous chat because after the product being recommended the user might want to purchase that.
+2. Also, create the table where the credit card info of the user including the product id should be saved into the database
+
    ....
--- clothShop.sql
+++ clothShop.sql
@@ -5,11 +5,13 @@ CREATE TABLE Products (
 	ProductID INT PRIMARY KEY,
 	ProductName TEXT,
 	ProductBrand TEXT,
-	Gender TEXT,
+	ProductSize VARCHAR(10),
+	Gender VARCHAR(10),
 	Price INT,
 	NumImages INT,
 	Description TEXT,
-	PrimaryColor TEXT
+	PrimaryColor VARCHAR(20),
+	ImageUrl TEXT
 );
 
 -- Query-2
@@ -18,95 +20,112 @@ INSERT INTO
 		ProductID,
 		ProductName,
 		ProductBrand,
+		ProductSize,
 		Gender,
 		Price,
 		NumImages,
 		Description,
-		PrimaryColor
+		PrimaryColor,
+		ImageUrl
 	)
 VALUES
 	(
 		10017413,
 		'Printed Medium Trolley Bag',
 		'DKNY',
+		'M',
 		'Unisex',
 		11745,
 		7,
 		'Black and grey printed medium trolley bag, secured with a TSA lock. One handle on the top and one on the side, has a trolley with a retractable handle on the top and four corner mounted inline skate wheels. One main zip compartment, zip lining, two compression straps with click clasps, one zip compartment on the flap with three zip pockets. Warranty: 5 years. Warranty provided by Brand Owner / Manufacturer',
-		'Black'
+		'Black',
+		'https://i.imgur.com/VTklGP8.png'
 	),
 	(
 		10016283,
 		'Women Beige & Grey Kurta Set',
 		'EthnoVogue',
+		'L',
 		'Women',
 		5810,
 		7,
 		'Beige & Grey made to measure kurta with churidar and dupatta. Beige made to measure calf length kurta, has a V-neck, three-quarter sleeves, lightly padded on bust, flared hem, concealed zip closure. Grey solid made to measure churidar, drawstring closure. Grey net sequined dupatta, has printed taping. What is Made to Measure? Customised Kurta Set according to your Bust and Length. So please refer to the Size Chart to pick your perfect size. How to measure bust? Measure under your arms and around your chest to find your bust size in inches. How to measure Kurta length? Measure from shoulder till barefoot to find kurta length',
-		'Beige'
+		'Beige',
+		'https://i.imgur.com/VDzDTh7.jpeg'
 	),
 	(
 		10009781,
 		'SPYKAR Women Pink Cropped Jeans',
 		'SPYKAR',
+		'S',
 		'Women',
 		899,
 		7,
 		'Pink coloured wash 5-pocket high-rise cropped jeans, clean look, no fade, has a button and zip closure, and waistband with belt loops',
-		'Pink'
+		'Pink',
+		'https://i.imgur.com/7nQ7ofi.png'
 	),
 	(
 		10015921,
 		'Raymond Men Blue Bandhgala Suit',
 		'Raymond',
+		'M',
 		'Men',
 		5599,
 		5,
 		'Blue self-design bandhgala suit. Blue self-design bandhgala blazer, has a mandarin collar, single breasted with full button placket, long sleeves, three pockets, an attached lining and a double-vented back hem. Blue self-design mid-rise trousers, has a zip fly with a button and a hook-and-bar closure, four pockets, a waistband with belt loops',
-		'Blue'
+		'Blue',
+		'https://i.imgur.com/qTNSCJm.png'
 	),
 	(
 		10017833,
 		'Parx Men Brown Casual Shirt',
 		'Parx',
+		'L',
 		'Men',
 		759,
 		5,
 		'Brown and off-white printed casual shirt, has a spread collar, long sleeves, button placket, curved hem, one patch pocket',
-		'White'
+		'Brown',
+		'https://i.imgur.com/rJgzr4F.jpeg'
 	),
 	(
 		10014361,
 		'Men Brown Solid Slim Fit Regular Shorts',
 		'SHOWOFF',
+		'L',
 		'Men',
 		791,
 		5,
 		'Brown solid low-rise regular shorts, has four pockets, a button closure',
-		'Brown'
+		'Brown',
+		'https://i.imgur.com/XqIy8vE.png'
+	),
+	(
+		10017900,
+		'Men Black Solid Slim Fit Regular Shorts',
+		'SHOWOFF',
+		'XL',
+		'Men',
+		791,
+		5,
+		'Black solid low-rise regular shorts, has four pockets, a button closure',
+		'Black',
+		'https://i.imgur.com/Uf3MmBg.png'
+	),
+	(
+		10017901,
+		'Pink Men Solid Slim Fit Regular Shorts',
+		'SHOWOFF',
+		'S',
+		'Men',
+		791,
+		5,
+		'Pink regular shorts, has four pockets, a button closure',
+		'Pink',
+		'https://i.imgur.com/Yu5ita5.png'
 	);
 
-(
-	10017900,
-	'Men Brown Solid Slim Fit Regular Shorts',
-	'SHOWOFF',
-	'Men',
-	791,
-	5,
-	'Brown solid low-rise regular shorts, has four pockets, a button closure',
-	'Black'
-);
-
-(
-	10017901,
-	'Pink Men Brown Solid Slim Fit Regular Shorts',
-	'SHOWOFF',
-	'Men',
-	791,
-	5,
-	'Brown solid low-rise regular shorts, has four pockets, a button closure',
-	'Pink'
-);
 
 -- Query for customer
 CREATE TABLE Customers (
@@ -196,3 +215,9 @@ VALUES
 		150,
 		'Processing'
 	);
+	
+	select * from customers;
+	select * from orders;
+	select * from products;
+	
+
